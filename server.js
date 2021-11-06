e1fa101bd284f6124e0f1145d1857c02222c37d6
nguyen-william93 <=>
2021-11-05 23:13:12 -0500

    restructure server.js and add comment for readability

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(routes)
+
 
 //handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 //session
 const session = require('express-session');
@@ -20,13 +17,23 @@ const sess = {
     secret: 'Super secret secret',
     resave: false,
     saveUninitialized: true,
+    cookie: {},
+    store: new SequalizeStore({
+        db:sequelize
+    })
 };
-app.use(session(sess));
 
+//initate app and set up app
+const app = express();
+app.use(routes)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(session(sess));
 
+//start a connection through sequelize
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
 });

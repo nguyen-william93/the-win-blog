3d3dbfbf72a5e66040b6d7de60b857edac79bb79
nguyen-william93 <=>
2021-11-08 22:59:45 -0600

    update models and index.js to set the relationship between tables

---
+++ models/index.js
@@ -0,0 +1,34 @@
+// import all models
+const Post = require('./post');
+const User = require('./user');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "SET NULL"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "SET NULL"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "SET NULL"
+});
+
+module.exports = { User, Post, Comment}
